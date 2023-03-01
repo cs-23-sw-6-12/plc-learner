917c6d908f85af8158c1a8b55217fd0a38c8a671
Simon Laursen <69715502+Laursen79@users.noreply.github.com>
2023-03-01 09:22:04 +0100

    basic active learning setup

--- src/main/java/org/cs_23_sw_6_12/Main.java
+++ src/main/java/org/cs_23_sw_6_12/Main.java
@@ -1,7 +1,41 @@
 package org.cs_23_sw_6_12;
+import de.learnlib.algorithms.dhc.mealy.MealyDHC;
+import de.learnlib.api.SUL;
+import de.learnlib.api.oracle.EquivalenceOracle;
+import de.learnlib.oracle.equivalence.mealy.RandomWalkEQOracle;
+import de.learnlib.oracle.membership.SULOracle;
+import de.learnlib.util.Experiment;
+import net.automatalib.automata.transducers.MealyMachine;
+import net.automatalib.visualization.Visualization;
+import net.automatalib.words.Alphabet;
+import net.automatalib.words.impl.Alphabets;
+
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int MAX_DEPTH = 3;
+        Alphabet<String> alphabet = null;
+
+        // System under learning.
+        SULTimed<String, String> sul = null;
+
+        // Standard mealy membership oracle.
+        SULOracle<String, String> membershipOracle = new SULOracle<>(sul);
+
+        EquivalenceOracle.MealyEquivalenceOracle<String,String> equivalenceOracle =
+                new ClockExplorationEQOracle<>(sul, MAX_DEPTH);
+
+        MealyDHC<String, String> learner = new MealyDHC<>(alphabet, membershipOracle);
+
+        Experiment.MealyExperiment<String, String> experiment =
+                new Experiment.MealyExperiment<>(learner, equivalenceOracle,alphabet);
+
+        experiment.run();
+
+        MealyMachine<?, String, ?, String> result = experiment.getFinalHypothesis();
+
+        Visualization.visualize(result, alphabet);
     }
 }
