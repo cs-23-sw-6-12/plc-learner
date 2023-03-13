7e4ed31449981a1c694ad23db2562d5b2e45fc47
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-03-13 11:32:06 +0100

    init truthtable

--- src/main/java/org/cs_23_sw_6_12/Main.java
+++ src/main/java/org/cs_23_sw_6_12/Main.java
@@ -1,22 +1,27 @@
 package org.cs_23_sw_6_12;
 import de.learnlib.algorithms.dhc.mealy.MealyDHC;
-import de.learnlib.api.oracle.EquivalenceOracle;
+import de.learnlib.driver.util.MealySimulatorSUL;
+import de.learnlib.oracle.equivalence.MealySimulatorEQOracle;
 import de.learnlib.oracle.membership.SULOracle;
 import de.learnlib.util.Experiment;
 import net.automatalib.automata.transducers.MealyMachine;
+import net.automatalib.automata.transducers.impl.compact.CompactMealyTransition;
 import net.automatalib.visualization.Visualization;
-import net.automatalib.words.Alphabet;
-import net.automatalib.words.impl.Alphabets;
-import org.cs_23_sw_6_12.Interfaces.SULTimed;
+import net.automatalib.words.Word;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
+/*
+        MealyMachine example = ExampleSUL.createExample();
         int MAX_DEPTH = 3;
 
-        Alphabet<Boolean[]> alphabet = Alphabets.fromArray(
+        TruthTable t = new TruthTable(example, ExampleSUL.alphabet);
+        t.generateTable();
+        Visualization.visualize(example, ExampleSUL.alphabet);
+
+        Alphabet<I> alphabet = Alphabets.fromArray(
                 new Boolean[]{true, false},
                 new Boolean[]{true, true},
                 new Boolean[]{false, true},
@@ -26,24 +31,29 @@ public class Main {
         // System under learning. TODO: Change to example
         SULClient sul = SULClient.createBooleanArrayClient(
                 new SULClientConfiguration(args[0], Integer.parseInt(args[1])));
+
         sul.numberofinputs = 2;
         sul.numberofoutputs = 2;
-
+ */
+        MealyMachine<Integer, Word<Boolean>, CompactMealyTransition<Object>, Object> example = ExampleSUL.createExample();
+        MealySimulatorSUL<Word<Boolean>, Object> sul = ExampleSUL.createExampleSUL();
         // Standard mealy membership oracle.
-        SULOracle<Boolean[], Boolean[]> membershipOracle = new SULOracle<>(sul);
+        SULOracle<Word<Boolean>, Object> membershipOracle = new SULOracle<>(sul);
 
-        EquivalenceOracle.MealyEquivalenceOracle<Boolean[], Boolean[]> equivalenceOracle =
-                new ClockExplorationEQOracle<>(sul, MAX_DEPTH);
+        MealySimulatorEQOracle<Word<Boolean>, Object> equivalenceOracle = new MealySimulatorEQOracle<>(example);
 
-        MealyDHC<Boolean[], Boolean[]> learner = new MealyDHC<>(alphabet, membershipOracle);
+        MealyDHC<Word<Boolean>, Object> learner = new MealyDHC<>(ExampleSUL.alphabet, membershipOracle);
 
-        Experiment.MealyExperiment<Boolean[], Boolean[]> experiment =
-                new Experiment.MealyExperiment<>(learner, equivalenceOracle, alphabet);
+        Experiment.MealyExperiment<Word<Boolean>, Object> experiment =
+                new Experiment.MealyExperiment<>(learner, equivalenceOracle, ExampleSUL.alphabet);
 
         experiment.run();
 
-        MealyMachine<?, Boolean[], ?, Boolean[]> result = experiment.getFinalHypothesis();
+        MealyMachine<?, Word<Boolean>, ?, Object> result = experiment.getFinalHypothesis();
 
-        Visualization.visualize(result, alphabet);
+        TruthTable t = new TruthTable(result, ExampleSUL.alphabet);
+        t.generateTable();
+        Visualization.visualize(result, ExampleSUL.alphabet);
+
     }
 }
