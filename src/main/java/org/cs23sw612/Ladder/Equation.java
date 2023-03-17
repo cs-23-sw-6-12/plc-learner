62880ada379d6b055142f30b04b4996229598785
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-03-17 12:32:30 +0100

    lol

--- src/main/java/org/cs23sw612/Ladder/Equation.java
+++ src/main/java/org/cs23sw612/Ladder/Equation.java
@@ -18,57 +18,57 @@ import org.checkerframework.checker.nullness.qual.NonNull;
  *            Output
  */
 public class Equation<S, I, O> {
-	public final O output;
-	private List<Pair<I, S>> or;
+    public final O output;
+    private List<Pair<I, S>> or;
 
-	public Equation(@NonNull O output) {
-		this.output = output;
-	}
+    public Equation(@NonNull O output) {
+        this.output = output;
+    }
 
-	/**
-	 * @param o
-	 *            A {@link EquationTable.EquationRow} to be augmented to an
-	 *            {@link Equation}
-	 */
-	public Equation(EquationTable.EquationRow<S, I, O> o) {
-		assert o.out() != null;
-		this.output = o.out();
-		or = new ArrayList<>();
-		or.add(Pair.of(o.ins(), o.states()));
-	}
+    /**
+     * @param o
+     *            A {@link EquationTable.EquationRow} to be augmented to an
+     *            {@link Equation}
+     */
+    public Equation(EquationTable.EquationRow<S, I, O> o) {
+        assert o.out() != null;
+        this.output = o.out();
+        or = new ArrayList<>();
+        or.add(Pair.of(o.ins(), o.states()));
+    }
 
-	/**
-	 * Method used to extend how the output can be produced. Can be viewed as a pair
-	 * ({@code in,s}) that (also) produces {@code out}.
-	 *
-	 * @param in
-	 *            The input to extend {@code out} with
-	 * @param s
-	 *            The state to extend {@code out} with
-	 */
-	public void extend(I in, S s) {
-		if (or == null)
-			or = new ArrayList<>();
-		or.add(Pair.of(in, s));
-	}
+    /**
+     * Method used to extend how the output can be produced. Can be viewed as a pair
+     * ({@code in,s}) that (also) produces {@code out}.
+     *
+     * @param in
+     *            The input to extend {@code out} with
+     * @param s
+     *            The state to extend {@code out} with
+     */
+    public void extend(I in, S s) {
+        if (or == null)
+            or = new ArrayList<>();
+        or.add(Pair.of(in, s));
+    }
 
-	public List<Pair<I, S>> getFullList() {
-		return or;
-	}
+    public List<Pair<I, S>> getFullList() {
+        return or;
+    }
 
-	// TODO: Formatting shit?
-	@Override
-	public String toString() {
-		return output.toString() + " = " + String.join(" + ",
-				or.stream().map(p -> String.format("(%s %s)", p.getFirst(), p.getSecond())).toList());
-	}
+    // TODO: Formatting shit?
+    @Override
+    public String toString() {
+        return output.toString() + " = " + String.join(" + ",
+                or.stream().map(p -> String.format("(%s %s)", p.getFirst(), p.getSecond())).toList());
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		return o instanceof EquationTable.EquationRow && equals((EquationTable.EquationRow<S, I, O>) o);
-	}
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof EquationTable.EquationRow && equals((EquationTable.EquationRow<S, I, O>) o);
+    }
 
-	private boolean equals(EquationTable.EquationRow<S, I, O> o) {
-		return o.out() == output;
-	}
+    private boolean equals(EquationTable.EquationRow<S, I, O> o) {
+        return o.out() == output;
+    }
 }
