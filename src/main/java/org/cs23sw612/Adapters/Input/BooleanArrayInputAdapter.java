f3fefdad0b2c47f86cef373922d8f203e84fcb2c
Thomas Lohse <49527735+t-lohse@users.noreply.github.com>
2023-03-17 12:37:41 +0100

    Spaces (#10)

--- src/main/java/org/cs23sw612/Adapters/Input/BooleanArrayInputAdapter.java
+++ src/main/java/org/cs23sw612/Adapters/Input/BooleanArrayInputAdapter.java
@@ -3,14 +3,14 @@ package org.cs23sw612.Adapters.Input;
 import org.cs23sw612.Adapters.InputAdapter;
 
 public class BooleanArrayInputAdapter implements InputAdapter<Boolean[]> {
-	@Override
-	public byte[] toBytes(Boolean[] object) {
-		byte[] output = new byte[object.length];
+    @Override
+    public byte[] toBytes(Boolean[] object) {
+        byte[] output = new byte[object.length];
 
-		for (int i = 0; i < object.length; i++) {
-			output[i] = (byte) (object[i] ? 1 : 0);
-		}
+        for (int i = 0; i < object.length; i++) {
+            output[i] = (byte) (object[i] ? 1 : 0);
+        }
 
-		return output;
-	}
+        return output;
+    }
 }
--- src/main/java/org/cs23sw612/SULWrapper.java
+++ src/main/java/org/cs23sw612/SULWrapper.java
@@ -4,30 +4,30 @@ import de.learnlib.api.SUL;
 
 public class SULWrapper<I, O> implements SUL<I, O> {
 
-	private final SUL<I, O> sul;
-	private long counter = 0;
+    private final SUL<I, O> sul;
+    private long counter = 0;
 
-	public SULWrapper(SUL<I, O> sul) {
-		this.sul = sul;
-	}
+    public SULWrapper(SUL<I, O> sul) {
+        this.sul = sul;
+    }
 
-	public long getCounter() {
-		return counter;
-	}
+    public long getCounter() {
+        return counter;
+    }
 
-	@Override
-	public void pre() {
-		counter++;
-		sul.pre();
-	}
+    @Override
+    public void pre() {
+        counter++;
+        sul.pre();
+    }
 
-	@Override
-	public void post() {
-		sul.post();
-	}
+    @Override
+    public void post() {
+        sul.post();
+    }
 
-	@Override
-	public O step(I i) {
-		return sul.step(i);
-	}
+    @Override
+    public O step(I i) {
+        return sul.step(i);
+    }
 }
