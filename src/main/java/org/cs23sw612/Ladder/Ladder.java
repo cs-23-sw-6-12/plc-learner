21380bc70e59ac8c2acefa6996f481cc9cd567d7
kspe20 <kspe20@student.aau.dk>
2023-03-23 12:35:08 +0100

    Made ladder with rungs consisting of gates, state and output

--- src/main/java/org/cs23sw612/Ladder/Ladder.java
+++ src/main/java/org/cs23sw612/Ladder/Ladder.java
@@ -16,6 +16,7 @@ public class Ladder<S, I extends Word<?>, T extends CompactMealyTransition<O>, O
         for (Equation<S, I, O> equation : ec) {
             for (Pair<I, S> vari : equation.getFullList()) {
                 Rung rung = new Rung();
+                int i = 1;
                 for (Object w : vari.getFirst()) {
                     String gatetype;
                     if (w.toString() == "true") {
@@ -23,18 +24,19 @@ public class Ladder<S, I extends Word<?>, T extends CompactMealyTransition<O>, O
                     } else {
                         gatetype = "NC";
                     }
-                    Gate gate = new Gate(gatetype);
+                    Gate gate = new Gate(gatetype, Integer.toString(i));
                     rung.gates.add(gate);
+                    i++;
                 }
+                Gate stategate = new Gate("NO", "State " + vari.getSecond().toString());
+                rung.gates.add(stategate);
+                Gate outputgate = new Gate("Coil", equation.output.toString());
+                rung.gates.add(outputgate);
                 rung.outputvar = equation.output;
                 rungs.add(rung);
             }
-
+            System.out.println(equation);
             System.out.println(rungs);
-            // System.out.println(equation);
-            // System.out.println(String.join("---",
-            // equation.getFullList().stream().map(Object::toString).toList()));
-
         }
 
     }
@@ -42,6 +44,16 @@ public class Ladder<S, I extends Word<?>, T extends CompactMealyTransition<O>, O
     private class Rung {
         O outputvar;
         ArrayList<Gate> gates = new ArrayList<Gate>();
+        @Override
+        public String toString(){
+            String start = "|---";
+            for (Gate gate : gates){
+                String g = gate.toString() + "---";
+                start = start + g;
+            }
+            start = start + "|";
+            return start;
+        }
 
     }
 
@@ -49,8 +61,16 @@ public class Ladder<S, I extends Word<?>, T extends CompactMealyTransition<O>, O
         String Inputvar;
         String Type;
 
-        public Gate(String type) {
+        public Gate(String type, String inputvar) {
             this.Type = type;
+            this.Inputvar = inputvar;
+        }
+        @Override
+        public String toString(){
+            return  Type + ":"+ Inputvar;
         }
     }
 }
+
+//Joms |--|1|---|/2|---|state?|---(? ?)---| or
+//Den her opgave er alt for meget string formatting lol i mean tru lol
