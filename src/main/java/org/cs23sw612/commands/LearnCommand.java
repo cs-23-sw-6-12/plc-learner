79f1df500f1b43bcbdde11223f23ba0ec2ded30b
Jens Emil Fink Højriis <5774453+JE-FH@users.noreply.github.com>
2023-04-04 15:20:23 +0200

    added savine learnt model

--- src/main/java/org/cs23sw612/commands/LearnCommand.java
+++ src/main/java/org/cs23sw612/commands/LearnCommand.java
@@ -4,6 +4,8 @@ import de.learnlib.filter.cache.sul.SULCache;
 import de.learnlib.oracle.equivalence.CompleteExplorationEQOracle;
 import de.learnlib.oracle.membership.SULOracle;
 import de.learnlib.util.Experiment;
+import net.automatalib.graphs.Graph;
+import net.automatalib.serialization.dot.DOTSerializationProvider;
 import net.automatalib.visualization.Visualization;
 import net.automatalib.words.Word;
 import org.cs23sw612.Adapters.Input.IntegerWordInputAdapter;
@@ -13,7 +15,7 @@ import org.cs23sw612.SULClientConfiguration;
 import org.cs23sw612.Util.AlphabetUtil;
 import org.cs23sw612.Util.LearnerFactoryRepository;
 import picocli.CommandLine;
-
+import java.io.FileOutputStream;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "learn", mixinStandardHelpOptions = true, version = "0.1.0", description = "Learns a PLCs logic")
@@ -34,8 +36,24 @@ public class LearnCommand implements Callable<Integer> {
     @CommandLine.Option(names = {"--learner",
             "-l"}, description = "Learner to use (case insensitive)", defaultValue = "DHC", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
     private String learnerName;
+
+    @CommandLine.Option(names = {"--visualize", "-v"}, description = "Visualize the automaton when done")
+    private boolean visualize;
+
+    @CommandLine.Option(names = {"--out", "-o"}, description = "Where the learned automaton should be saved", defaultValue = "automaton.json", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+    private String outputFileName;
+
     @Override
     public Integer call() throws Exception {
+        FileOutputStream outFile;
+        try {
+            outFile = new FileOutputStream(outputFileName);
+        } catch (Exception ex) {
+            System.err.println("Could not open output file for writing");
+            System.err.println(ex.getMessage());
+            return 1;
+        }
+
         var sul = SULClient.createClient(new SULClientConfiguration(bajerServerAddress, bajerServerPort),
                 new IntegerWordInputAdapter(), new IntegerWordOutputAdapter());
         sul.numberofinputs = inputCount;
@@ -63,13 +81,17 @@ public class LearnCommand implements Callable<Integer> {
 
         var learner = learnerFactory.createLearner(alphabet, membershipOracle);
 
-        var experiment = new Experiment.MealyExperiment<>(learner, equivalenceOracle, alphabet);
+        var experiment = new Experiment.MealyExperiment<Word<Integer>, Word<Integer>>(learner, equivalenceOracle, alphabet);
 
         experiment.run();
 
         var result = experiment.getFinalHypothesis();
 
-        Visualization.visualize(result, alphabet);
+        DOTSerializationProvider.getInstance().writeModel(outFile, (Graph) result.transitionGraphView(alphabet).asNormalGraph());
+
+        if (visualize) {
+            Visualization.visualize(result, alphabet);
+        }
 
         return 0;
     }
