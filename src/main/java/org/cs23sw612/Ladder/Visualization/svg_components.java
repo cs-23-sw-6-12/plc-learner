3632a022db0a6a101991370f533a066b41206ac7
kspe20 <kspe20@student.aau.dk>
2023-04-05 12:17:57 +0200

    Changed gate length

--- src/main/java/org/cs23sw612/Ladder/Visualization/svg_components.java
+++ src/main/java/org/cs23sw612/Ladder/Visualization/svg_components.java
@@ -1,47 +1,49 @@
 package org.cs23sw612.Ladder.Visualization;
 
+import javax.swing.text.JTextComponent;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Path2D;
 import java.awt.geom.Rectangle2D;
 
 public class svg_components {
-    public static void NO(Graphics2D g, int x, int y){
+   
+    public static void NO(Graphics2D g, double x, double y){
         Path2D path = new Path2D.Double();
         path.moveTo(x,y);
-        path.lineTo(x+5,y);
-        path.moveTo(x+5, y-15);
-        path.lineTo(x+5, y+15);
-        path.moveTo(x+15, y-15);
-        path.lineTo(x+15, y+15);
-        path.moveTo(x+15, y);
-        path.lineTo(x+20, y);
+        path.lineTo(x+6.25,y);
+        path.moveTo(x+6.25, y-15);
+        path.lineTo(x+6.25, y+15);
+        path.moveTo(x+18.75, y-15);
+        path.lineTo(x+18.75, y+15);
+        path.moveTo(x+18.75, y);
+        path.lineTo(x+25, y);
         g.draw(path);
     }
-    public static void NC(Graphics2D g, int x, int y){
+    public static void NC(Graphics2D g, double x, double y){
         Path2D path = new Path2D.Double();
         path.moveTo(x,y);
-        path.lineTo(x+5, y);
-        path.moveTo(x+5, y-15);
-        path.lineTo(x+5, y+15);
-        path.moveTo(x+5.2, y+14.9);
-        path.lineTo(x+14.8,y-14.9);
-        path.moveTo(x+15,y-15);
-        path.lineTo(x+15, y+15);
-        path.moveTo(x+15, y);
-        path.lineTo(x+20, y);
+        path.lineTo(x+6.25, y);
+        path.moveTo(x+6.25, y-15);
+        path.lineTo(x+6.25, y+15);
+        path.moveTo(x+6.45, y+14.9);
+        path.lineTo(x+18.55,y-14.9);
+        path.moveTo(x+18.75,y-15);
+        path.lineTo(x+18.75, y+15);
+        path.moveTo(x+18.75, y);
+        path.lineTo(x+25, y);
         g.draw(path);
     }
-    public static void Coil(Graphics2D g2, int x, int y) {
+    public static void Coil(Graphics2D g2, double x, double y) {
         Path2D path = new Path2D.Double();
         path.moveTo(x,y);
         path.lineTo(x+2.5,y);
         path.moveTo(x+7.5, y-15);
         path.curveTo(x+7.5,y-15,x-3.8,y,x+7.5,y+15);
-        path.moveTo(x+12.5,y-15);
-        path.curveTo(x+12.5,y-15,x+23.8,y,x+12.5,y+15);
-        path.moveTo(x+17.5,y);
-        path.lineTo(x+20,y);
+        path.moveTo(x+17.5,y-15);
+        path.curveTo(x+17.5,y-15,x+28.8,y,x+17.5,y+15);
+        path.moveTo(x+22.5,y);
+        path.lineTo(x+25,y);
         g2.draw(path);
     }
 }
--- src/main/java/org/cs23sw612/Main.java
+++ src/main/java/org/cs23sw612/Main.java
@@ -6,6 +6,7 @@ import de.learnlib.oracle.equivalence.CompleteExplorationEQOracle;
 import de.learnlib.oracle.membership.SULOracle;
 import de.learnlib.util.Experiment;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import net.automatalib.visualization.Visualization;
@@ -19,36 +20,30 @@ import static org.cs23sw612.Ladder.Visualization.svg_components.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-          SVGGraphics2D g2 = new SVGGraphics2D(110, 150);
-          NO(g2,25,30);
-          NC(g2,25,65);
-          Coil(g2,25,100);
-          SVGUtils.writeToSVG(new File("test_image.svg"), g2.getSVGElement());
+        var sul = SULClient.createClient(new SULClientConfiguration(args[0], Integer.parseInt(args[1])),
+                new IntegerWordInputAdapter(), new IntegerWordOutputAdapter());
+        sul.numberofinputs = 3;
+        sul.numberofoutputs = 1;
 
-    //    var sul = SULClient.createClient(new SULClientConfiguration(args[0], Integer.parseInt(args[1])),
-    //            new IntegerWordInputAdapter(), new IntegerWordOutputAdapter());
-    //    sul.numberofinputs = 3;
-    //    sul.numberofoutputs = 1;
-    //
-    //    var alphabet = AlphabetUtil.createIntAlphabet(3);
-    //
-    //    // This cache reduces the amount of "queries" that are actually executed on the
-    //    // SUL.
-    //    var cache = SULCache.createTreeCache(alphabet, sul);
-    //
-    //    // Standard mealy membership oracle.
-    //    var membershipOracle = new SULOracle<>(cache);
-    //
-    //    var equivalenceOracle = new CompleteExplorationEQOracle<>(membershipOracle, 3);
-    //
-    //    var learner = new MealyDHC<>(alphabet, membershipOracle);
-    //
-    //    var experiment = new Experiment.MealyExperiment<>(learner, equivalenceOracle, alphabet);
-    //
-    //    experiment.run();
-    //
-    //    var result = experiment.getFinalHypothesis();
-    //
-    //    Visualization.visualize(result, alphabet);
+        var alphabet = AlphabetUtil.createIntAlphabet(3);
+
+        // This cache reduces the amount of "queries" that are actually executed on the
+        // SUL.
+        var cache = SULCache.createTreeCache(alphabet, sul);
+
+        // Standard mealy membership oracle.
+        var membershipOracle = new SULOracle<>(cache);
+
+        var equivalenceOracle = new CompleteExplorationEQOracle<>(membershipOracle, 3);
+
+        var learner = new MealyDHC<>(alphabet, membershipOracle);
+
+        var experiment = new Experiment.MealyExperiment<>(learner, equivalenceOracle, alphabet);
+
+        experiment.run();
+
+        var result = experiment.getFinalHypothesis();
+
+        Visualization.visualize(result, alphabet);
     }
 }
