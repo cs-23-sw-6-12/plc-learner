cf52cfb1769e00a769089dae1383bc74714f082a
kspe20 <kspe20@student.aau.dk>
2023-04-05 12:33:14 +0200

    Deleted svg_components class and added it their respective classes

--- src/main/java/org/cs23sw612/Ladder/Visualization/SVGCoil.java
+++ src/main/java/org/cs23sw612/Ladder/Visualization/SVGCoil.java
@@ -18,10 +18,10 @@ public class SVGCoil extends SVGRungElement {
         path.lineTo(x+2.5,y);
         path.moveTo(x+7.5, y-15);
         path.curveTo(x+7.5,y-15,x-3.8,y,x+7.5,y+15);
-        path.moveTo(x+12.5,y-15);
-        path.curveTo(x+12.5,y-15,x+23.8,y,x+12.5,y+15);
-        path.moveTo(x+17.5,y);
-        path.lineTo(x+20,y);
+        path.moveTo(x+17.5,y-15);
+        path.curveTo(x+17.5,y-15,x+28.8,y,x+17.5,y+15);
+        path.moveTo(x+22.5,y);
+        path.lineTo(x+25,y);
         return path;
     }
 }
