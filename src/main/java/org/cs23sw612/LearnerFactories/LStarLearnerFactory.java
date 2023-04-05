be0c0aa4780fe1424f885f58aed2210cf0425d34
Jens Emil Fink Højriis <5774453+JE-FH@users.noreply.github.com>
2023-04-05 11:09:19 +0200

    fixed l* learner factory

--- src/main/java/org/cs23sw612/LearnerFactories/LStarLearnerFactory.java
+++ src/main/java/org/cs23sw612/LearnerFactories/LStarLearnerFactory.java
@@ -1,9 +1,13 @@
 package org.cs23sw612.LearnerFactories;
 
 import de.learnlib.acex.analyzers.AcexAnalyzers;
+import de.learnlib.algorithms.lstar.mealy.ExtensibleLStarMealy;
+import de.learnlib.algorithms.lstar.mealy.ExtensibleLStarMealyBuilder;
+import de.learnlib.algorithms.lstar.mealy.LStarMealyUtil;
 import de.learnlib.algorithms.ttt.mealy.TTTLearnerMealy;
 import de.learnlib.api.algorithm.LearningAlgorithm;
 import de.learnlib.oracle.membership.SULOracle;
+import de.learnlib.util.mealy.MealyUtil;
 import net.automatalib.words.Alphabet;
 import org.cs23sw612.Interfaces.MealyLearnerFactory;
 
@@ -15,6 +19,10 @@ public class LStarLearnerFactory<I, O> implements MealyLearnerFactory<I, O> {
 
     @Override
     public LearningAlgorithm.MealyLearner<I, O> createLearner(Alphabet<I> alphabet, SULOracle<I, O> membershipOracle) {
-        return new TTTLearnerMealy<>(alphabet, membershipOracle, AcexAnalyzers.BINARY_SEARCH_BWD);
+
+        return new ExtensibleLStarMealyBuilder<I, O>()
+                .withAlphabet(alphabet)
+                .withOracle(membershipOracle)
+                .create();
     }
 }
