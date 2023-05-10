c46adf2b36a99f1bd548b190cf811a54e03ab1be
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-05-10 11:35:01 +0200

    fix

--- src/main/java/org/cs23sw612/Util/CacheSULFactory.java
+++ src/main/java/org/cs23sw612/Util/CacheSULFactory.java
@@ -4,7 +4,7 @@ import de.learnlib.api.SUL;
 import net.automatalib.words.Word;
 
 public class CacheSULFactory {
-    public static SUL<Word<Integer>, Word<Integer>> create(String cachePath, SUL<Word<Integer>, Word<Integer>> sul) {
+    public static SUL<Word<Boolean>, Word<Boolean>> create(String cachePath, SUL<Word<Boolean>, Word<Boolean>> sul) {
         return null;
     }
 }
