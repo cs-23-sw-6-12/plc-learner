e0a219df21176e4c09804f7eba372cb90cedb29a
Thomas Lohse <49527735+t-lohse@users.noreply.github.com>
2023-05-19 10:53:57 +0200

    Cleans (#29)

--- src/main/java/org/cs23sw612/SUL/SULClient.java
+++ src/main/java/org/cs23sw612/SUL/SULClient.java
@@ -1,15 +1,14 @@
 package org.cs23sw612.SUL;
 
-import de.learnlib.api.exception.SULException;
+import de.learnlib.api.SUL;
 
 import org.cs23sw612.Adapters.InputAdapter;
 import org.cs23sw612.Adapters.OutputAdapter;
 import org.cs23sw612.BAjER.IBAjERClient;
-import org.cs23sw612.Interfaces.SULTimed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SULClient<I, IA extends InputAdapter<I>, O, OA extends OutputAdapter<O>> implements SULTimed<I, O> {
+public class SULClient<I, IA extends InputAdapter<I>, O, OA extends OutputAdapter<O>> implements SUL<I, O> {
     private final Logger logger = LoggerFactory.getLogger(SULClient.class);
     private byte inputCount;
     private byte outputCount;
@@ -26,16 +25,6 @@ public class SULClient<I, IA extends InputAdapter<I>, O, OA extends OutputAdapte
 
         this.inputCount = inputCount;
         this.outputCount = outputCount;
-    }
-
-    @Override
-    public O step(I input, long stepClockLimit) throws SULException {
-        return null;
-    }
-
-    @Override
-    public long getClockLimit() {
-        return 0;
     }
 
     @Override
