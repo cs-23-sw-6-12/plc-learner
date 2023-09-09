12b3008f6679c345832973542b9e5ea8a419cbfd
kspe20 <kspe20@student.aau.dk>
2023-09-09 09:58:21 +0200

    Resolved comment about hardcoded values in NewSVGGate.java & NewSVGCoil.java

--- src/main/java/org/cs23sw612/Ladder/Visualization/SVG/NewSVGCoil.java
+++ src/main/java/org/cs23sw612/Ladder/Visualization/SVG/NewSVGCoil.java
@@ -3,6 +3,15 @@ package org.cs23sw612.Ladder.Visualization.SVG;
 import java.awt.geom.Path2D;
 
 public class NewSVGCoil extends NewSVGRungElement {
+    private final double horizontalLineLength = 2.5;
+    private final double leftCurveStartAndEndXValue = 7.5;
+    private final double leftCurveXValue = 3.8;
+    private final double curveYValue = 15;
+    private final double rightCurveStartAndEndXValue = 17.5;
+    private final double rightCurveXValue = 28.8;
+    private final double rightHorizontalLineStartPointX = 22.5;
+    private final double rightHorizontalLineEndPointx = rightHorizontalLineStartPointX + horizontalLineLength;
+
 
     public NewSVGCoil(Double x, Double y, String text) {
         super(x, y, text);
@@ -12,13 +21,13 @@ public class NewSVGCoil extends NewSVGRungElement {
     public Path2D getShape(double x, double y) {
         Path2D path = new Path2D.Double();
         path.moveTo(x, y);
-        path.lineTo(x + 2.5, y);
-        path.moveTo(x + 7.5, y - 15);
-        path.curveTo(x + 7.5, y - 15, x - 3.8, y, x + 7.5, y + 15);
-        path.moveTo(x + 17.5, y - 15);
-        path.curveTo(x + 17.5, y - 15, x + 28.8, y, x + 17.5, y + 15);
-        path.moveTo(x + 22.5, y);
-        path.lineTo(x + 25, y);
+        path.lineTo(x + horizontalLineLength, y);
+        path.moveTo(x + leftCurveStartAndEndXValue, y - curveYValue);
+        path.curveTo(x + leftCurveStartAndEndXValue, y - curveYValue, x - leftCurveXValue, y, x + leftCurveStartAndEndXValue, y + curveYValue);
+        path.moveTo(x + rightCurveStartAndEndXValue, y - curveYValue);
+        path.curveTo(x + rightCurveStartAndEndXValue, y - curveYValue, x + rightCurveXValue, y, x + rightCurveStartAndEndXValue, y + curveYValue);
+        path.moveTo(x + rightHorizontalLineStartPointX, y);
+        path.lineTo(x + rightHorizontalLineEndPointx, y);
         return path;
     }
 }
--- src/main/java/org/cs23sw612/Ladder/Visualization/SVG/NewSVGGate.java
+++ src/main/java/org/cs23sw612/Ladder/Visualization/SVG/NewSVGGate.java
@@ -4,26 +4,35 @@ import java.awt.geom.Path2D;
 
 public class NewSVGGate extends NewSVGRungElement {
     boolean open;
+    private final double horizontalLineLength = 6.25;
+    private final double verticalLineHalfLength = 15;
+    private final double rightVerticalLineX = 18.75;
+    private final double diagonaleLineStartPointX = 6.45;
+    private final double diagonaleLineStartPointY = 14.9;
+    private final double diagonaleLineEndPointX = 18.55;
+    private final double diagonaleLineEndPointY = 14.9;
+    private final double rightHorizontalLineEndPointX = rightVerticalLineX + horizontalLineLength;
+
     public NewSVGGate(double gateNumber, double rungNumber, String text, boolean open) {
         super(gateNumber, rungNumber, text);
         this.open = open;
     }
 
-    public Path2D getShape(double x, double y) { //Todo lav linjen i starten og slutningen længere :)
+    public Path2D getShape(double x, double y) {
         Path2D path = new Path2D.Double();
 
         path.moveTo(x, y);
-        path.lineTo(x + 6.25, y);
-        path.moveTo(x + 6.25, y - 15);
-        path.lineTo(x + 6.25, y + 15);
+        path.lineTo(x + horizontalLineLength, y);
+        path.moveTo(x + horizontalLineLength, y - verticalLineHalfLength);
+        path.lineTo(x + horizontalLineLength, y + verticalLineHalfLength);
         if (!open) {
-            path.moveTo(x + 6.45, y + 14.9);
-            path.lineTo(x + 18.55, y - 14.9);
+            path.moveTo(x + diagonaleLineStartPointX, y + diagonaleLineStartPointY);
+            path.lineTo(x + diagonaleLineEndPointX, y - diagonaleLineEndPointY);
         }
-        path.moveTo(x + 18.75, y - 15);
-        path.lineTo(x + 18.75, y + 15);
-        path.moveTo(x + 18.75, y);
-        path.lineTo(x + 25, y);
+        path.moveTo(x + rightVerticalLineX, y - verticalLineHalfLength);
+        path.lineTo(x + rightVerticalLineX, y + verticalLineHalfLength);
+        path.moveTo(x + rightVerticalLineX, y);
+        path.lineTo(x + rightHorizontalLineEndPointX, y);
         return path;
     }
 }
